7c2256c790f4bd74d6fa50aa13b958fb824512af
Mehathab06 <shaikmehathab20@gmail.com>
2026-05-04 15:16:44 +0530

    new changes

--- src/Average.java
+++ src/Average.java
@@ -5,7 +5,9 @@ public class Average {
         int num1 = 32;
         int num2 = 26;
         int num3 = 41;
-        int AverageOfThreeNumbers = num1 + num2+ num3 / 3;
+                int num4 = 55;
+        int AverageOfThreeNumbers = num1 + num2+ num3 + num4 / 4;
         System.out.println(AverageOfThreeNumbers);
+
     }
 }
